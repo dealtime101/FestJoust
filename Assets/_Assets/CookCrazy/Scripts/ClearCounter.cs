c279b9a6e3d75b0608994a671fe6ea17f2b5c237
dealtime101 <dealtime101@hotmail.com>
2024-03-31 00:34:49 -0400

    Kitchen Object Parent
    Testing Kitchen Object Transfert Counter

--- Assets/_Assets/CookCrazy/Scripts/ClearCounter.cs
+++ Assets/_Assets/CookCrazy/Scripts/ClearCounter.cs
@@ -6,32 +6,62 @@ using UnityEngine;
 // ce qui permet à ce script d'être attaché à un objet Unity.
 public class ClearCounter : MonoBehaviour
 {
-    // Utilise [SerializeField] pour exposer une variable privée dans l'inspecteur Unity sans rendre la variable publique.
-    // Cela permet de conserver l'encapsulation tout en permettant le réglage via l'éditeur Unity.
-    [SerializeField] private KitchenObjectSO kitchenObjectSO; // Référence à un ScriptableObject représentant un objet de cuisine.
-    [SerializeField] private Transform counterTopPoint; // Point de transformation sur le comptoir où les objets de cuisine seront placés.
 
-    // Variable privée pour suivre l'instance de KitchenObject actuellement sur le comptoir.
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
     private KitchenObject kitchenObject;
 
-    // Méthode publique Interact appelée pour interagir avec le comptoir.
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+                Debug.Log(kitchenObject.GetClearCounter());
+            }
+        }
+    }
+
     public void Interact()
     {
-        // Vérifie si kitchenObject est nul, ce qui indique qu'aucun objet n'est actuellement placé sur le comptoir.
         if (kitchenObject == null)
         {
-            // Si aucun objet n'est présent, un nouvel objet est instancié à la position counterTopPoint,
-            // en utilisant le prefab défini dans kitchenObjectSO.
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-
-            // Le positionnement local de l'objet instancié est réinitialisé à Vector3.zero,
-            // ce qui le place exactement à l'emplacement du point counterTopPoint.
-            kitchenObjectTransform.localPosition = Vector3.zero;
-
-            // Récupère le composant KitchenObject de l'objet instancié et le stocke dans la variable kitchenObject,
-            // permettant ainsi de garder une trace de l'objet actuellement placé sur le comptoir.
-            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
         }
 
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKlKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
