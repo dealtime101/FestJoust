cb5df68a198acd96d09b1f867a1aee1ecc2eb7a5
dealtime101 <dealtime101@hotmail.com>
2024-04-05 23:15:32 -0400

    Plates Counter (Update 2)
    Add ability to player to pick up plate.

--- Assets/_Assets/CookCrazy/Scripts/Counters/PlatesCounter.cs
+++ Assets/_Assets/CookCrazy/Scripts/Counters/PlatesCounter.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatesCounter : BaseCounter
 {
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
 
     [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
 
@@ -23,6 +27,23 @@ public class PlatesCounter : BaseCounter
             if (platesSpawnedAmount < platesSpawnedAmountMax)
             {
                 platesSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            if (platesSpawnedAmount > 0)
+            {
+                platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
             }
         }
     }
