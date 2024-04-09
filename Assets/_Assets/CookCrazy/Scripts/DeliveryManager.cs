d138456b150f1a5e92db5df682a3deb56b45b841
dealtime101 <dealtime101@hotmail.com>
2024-04-08 22:58:48 -0400

    Delivery Manager (Update 2)
    Finish Delivery Manager
    Add Mechanics of Delivery Quest.

--- Assets/_Assets/CookCrazy/Scripts/DeliveryManager.cs
+++ Assets/_Assets/CookCrazy/Scripts/DeliveryManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
-    [SerializeField] private RecipeListSO recipeListSO;
-    private List<RecipeSO> waitingRecipeSOList;
+    public static DeliveryManager Instance { get; private set; }
 
+
+    [SerializeField] private RecipeListSO recipeListSO;
+
+
+    private List<RecipeSO> waitingRecipeSOList;
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipeMax = 4;
@@ -14,6 +18,7 @@ public class DeliveryManager : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
         waitingRecipeSOList = new List<RecipeSO>();
     }
 
@@ -31,5 +36,44 @@ public class DeliveryManager : MonoBehaviour
                 waitingRecipeSOList.Add(waitingRecipeSO);
             }
         }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for (int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count)
+            {
+                bool plateContentsMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList)
+                {
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+                    {
+                        if (plateKitchenObjectSO == recipeKitchenObjectSO)
+                        {
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+                    if (!ingredientFound)
+                    {
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+
+                if (plateContentsMatchesRecipe)
+                {
+                    Debug.Log("Player delivered the correct recipe!");
+                    waitingRecipeSOList.RemoveAt(i);
+                    return;
+                }
+
+            }
+        }
+
+        Debug.Log("Player did not deliver a correct recipe");
     }
 }
